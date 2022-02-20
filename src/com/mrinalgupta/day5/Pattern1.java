ddff23629a117c9ad9c7d4f753d30e8661f177ed
Mrinal Gupta <mrinalgupta1097@gmail.com>
2022-02-20 12:28:33 +0530

    changes and right angle triangle pattern

--- src/com/mrinalgupta/day5/Pattern1.java
+++ src/com/mrinalgupta/day5/Pattern1.java
@@ -3,13 +3,13 @@ package com.mrinalgupta.day5;
 public class Pattern1 {
   public static void pyramidPattern(int n) {
     //      number of rows : outer loop
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
       //      print the Space
       System.out.println(" ");
 //      inner loop for columns
-      for (int j = 0; j <= i; j++) {
+      for (int j = 1; j <= i; j++) {
 //        print the star
-        System.out.println("* ");
+        System.out.println( j + " ");
       }
     }
 
